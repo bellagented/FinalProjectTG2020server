6478aeaffa335687542d6bdd81713a381f68bd77
bellagented <bellagented@gmail.com>
2021-01-13 10:56:46 +0100

    add game to chat

--- server.js
+++ server.js
@@ -23,12 +23,12 @@ io.on('connection', socket => {
   const id = socket.handshake.query.id
   socket.join(id)
 
-  socket.on('send-message', ({ recipients, text }) => {
+  socket.on('send-message', ({ recipients, game, text }) => {
     recipients.forEach(recipient => {
       const newRecipients = recipients.filter(r => r !== recipient)
       newRecipients.push(id)
       socket.broadcast.to(recipient).emit('receive-message', {
-        recipients: newRecipients, sender: id, text
+        recipients: newRecipients, game, sender: id, text
       })
     })
   })
