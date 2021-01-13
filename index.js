48da57be5fcf3c1076b0c55cef9e8bd757edc3c1
bellagented <bellagented@gmail.com>
2021-01-13 16:18:49 +0100

    reverse review

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-import dotenv  from "dotenv"
-import express from "express"
-import cors from "cors"
-import bp from "body-parser"
-import fetch from "node-fetch"
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import bp from "body-parser";
+import fetch from "node-fetch";
 // import Review from "./calls/Review.js"
 
+dotenv.config();
 
-dotenv.config()
-
-const app = express()
-const port = process.env.PORT || 4000
+const app = express();
+const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(bp.urlencoded({ extended: false }));
@@ -24,110 +23,116 @@ const AIRTABLETABLENAME = "VideogamingTable"; // table name
 // })
 
 app.get("/view", (req, res) => {
-
-  
-      fetch(
-        `https://api.airtable.com/v0/${AIRTABLEBASEID}/${AIRTABLETABLENAME}?view=Grid%20view`,
-        {
-          headers: { Authorization: `Bearer ${AIRTABLEAPI}` } 
-        }
-      )
-        .then((res) => res.json())
-        .then((result) => {
-          console.log(result);
-          res.json(result);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+  fetch(
+    `https://api.airtable.com/v0/${AIRTABLEBASEID}/${AIRTABLETABLENAME}?view=Grid%20view`,
+    {
+      headers: { Authorization: `Bearer ${AIRTABLEAPI}` },
+    }
+  )
+    .then((res) => res.json())
+    .then((result) => {
+      console.log(result);
+      res.json(result);
+    })
+    .catch((err) => {
+      console.log(err);
     });
+});
 
 app.post("/create", (req, res) => {
-        console.log(req.body);
-      
-        var datain = req.body;
-      
-        var payload = {
-          records: [
-            {
-              fields: datain,
-            },
-          ],
-        };
-      
-      //we need to send a "POST" request with our base id, table name, our API key, and send a body with the new data we wish to add.
-      
-        fetch(`https://api.airtable.com/v0/${AIRTABLEBASEID}/${AIRTABLETABLENAME}`, {
-          method: "post", // make sure it is a "POST request"
-          body: JSON.stringify(payload),
-          headers: {
-            Authorization: `Bearer ${AIRTABLEAPI}`,   // API key
-            "Content-Type": "application/json",  // we will recive a json object
-          },
-        })
-          .then((res) => res.json())
-          .then((result) => {
-            console.log(result);
-            res.json(result);
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      });    
+  console.log(req.body);
 
+  var datain = req.body;
+
+  var payload = {
+    records: [
+      {
+        fields: datain,
+      },
+    ],
+  };
+
+  //we need to send a "POST" request with our base id, table name, our API key, and send a body with the new data we wish to add.
+
+  fetch(`https://api.airtable.com/v0/${AIRTABLEBASEID}/${AIRTABLETABLENAME}`, {
+    method: "post", // make sure it is a "POST request"
+    body: JSON.stringify(payload),
+    headers: {
+      Authorization: `Bearer ${AIRTABLEAPI}`, // API key
+      "Content-Type": "application/json", // we will recive a json object
+    },
+  })
+    .then((res) => res.json())
+    .then((result) => {
+      console.log(result);
+      res.json(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 //backend review personali
-const AIRTABLETABLEREVIEW = "RecTable"
+const AIRTABLETABLEREVIEW = "RecTable";
 
-      app.route("/myrev/:name")
-      .get((req, res)=>{
-        
-        fetch(
-              `https://api.airtable.com/v0/${AIRTABLEBASEID}/${AIRTABLETABLEREVIEW}?view=Grid%20view`,
-              {
-                headers: { Authorization: `Bearer ${AIRTABLEAPI}` } 
-              }
-            )
-              .then((res) => res.json())
-              .then((tabledata)=>{
-                const myrec =tabledata.records.filter((rec)=>{return rec.fields.User===req.params.name}).map((r)=>{return {game: r.fields.Game, text:r.fields.Review}})
-              return myrec})
-              .then((result) => {
-                res.json(result);
-              })
-              .catch((err) => {
-                console.log(err);
-              });
-          }
-      )
-      .post((req, res)=>{ 
-        var datain = req.body;
-      
-        var payload = {
-          records: [
-            {
-              fields: datain,
-            },
-          ],
-        };
-        fetch(`https://api.airtable.com/v0/${AIRTABLEBASEID}/${AIRTABLETABLEREVIEW}`, {
+app
+  .route("/myrev/:name")
+  .get((req, res) => {
+    fetch(
+      `https://api.airtable.com/v0/${AIRTABLEBASEID}/${AIRTABLETABLEREVIEW}?view=Grid%20view`,
+      {
+        headers: { Authorization: `Bearer ${AIRTABLEAPI}` },
+      }
+    )
+      .then((res) => res.json())
+      .then((tabledata) => {
+        const myrec = tabledata.records
+          .filter((rec) => {
+            return rec.fields.User === req.params.name;
+          })
+          .map((r) => {
+            return { game: r.fields.Game, text: r.fields.Review };
+          })
+          .reverse();
+        return myrec;
+      })
+      .then((result) => {
+        res.json(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  })
+  .post((req, res) => {
+    var datain = req.body;
+
+    var payload = {
+      records: [
+        {
+          fields: datain,
+        },
+      ],
+    };
+    fetch(
+      `https://api.airtable.com/v0/${AIRTABLEBASEID}/${AIRTABLETABLEREVIEW}`,
+      {
         method: "post", // make sure it is a "POST request"
         body: JSON.stringify(payload),
         headers: {
-          Authorization: `Bearer ${AIRTABLEAPI}`,   // API key
-          "Content-Type": "application/json",  // we will recive a json object
+          Authorization: `Bearer ${AIRTABLEAPI}`, // API key
+          "Content-Type": "application/json", // we will recive a json object
         },
+      }
+    )
+      .then((res) => res.json())
+      .then((result) => {
+        res.json(result);
       })
-        .then((res) => res.json())
-        .then((result) => {
-          res.json(result);
-        })
-        .catch((err) => {
-          console.log(err);
-        });})
-
-
+      .catch((err) => {
+        console.log(err);
+      });
+  });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
